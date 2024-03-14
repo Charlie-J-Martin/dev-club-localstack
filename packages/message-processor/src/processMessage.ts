f519e36c5cde1063b285dc8bd89666081fa4161e
Charlie Martin <56304371+Charlie-J-Martin@users.noreply.github.com>
2024-03-14 15:47:26 +0000

    feat: message processor

---
+++ packages/message-processor/src/processMessage.ts
@@ -0,0 +1,25 @@
+import { deleteMessageFromSQS } from '../../sqs/src/deleteMessage';
+import { fetchMessagesToSQS } from '../../sqs/src/fetchMessage';
+
+export const processMessage = async (queueUrl: string) => {
+  const messages = await fetchMessagesToSQS(queueUrl);
+  if (messages) {
+    for (const message of messages) {
+      console.log(
+        'Processing order... \n',
+        'Order:',
+        JSON.parse(message.Body!)
+      );
+      await deleteMessageFromSQS(queueUrl, message.ReceiptHandle!);
+    }
+  } else {
+    console.log('No messages received');
+  }
+};
+
+export const pollQueue = async (queueUrl: string) => {
+  while (true) {
+    await processMessage(queueUrl);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+};
