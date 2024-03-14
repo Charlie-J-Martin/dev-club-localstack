9bfb87a09a61ac4f55e5c1360a03b4b65907fa57
Charlie Martin <56304371+Charlie-J-Martin@users.noreply.github.com>
2024-03-14 15:48:57 +0000

    refactor: For more order specific example

--- packages/message-generator/src/generateMessage.ts
+++ packages/message-generator/src/generateMessage.ts
@@ -1,11 +1,18 @@
+import { productData } from './products';
+
 type Message = {
   messageId: string;
   timeStamp: string;
   message: string;
 };
 
+export const pickRandomProduct = () => {
+  const randomIndex = Math.floor(Math.random() * productData.length);
+  return productData[randomIndex];
+};
+
 export const generateMessage = (): Message => ({
   messageId: Math.random().toString(36).substring(7),
   timeStamp: new Date().toISOString(),
-  message: 'SQS MESSAGES ARE COOL!',
+  message: JSON.stringify(pickRandomProduct()),
 });
--- packages/message-generator/src/main.ts
+++ packages/message-generator/src/main.ts
@@ -1,4 +1,4 @@
-import { sendMessage } from '../../sqs/src/sendMessage';
+import { sendMessageToSQS } from '../../sqs/src/sendMessage';
 import { generateMessage } from './generateMessage';
 
 const queueUrl = 'http://localhost:4566/000000000000/dev-club-localstack-queue';
@@ -6,7 +6,8 @@ const queueUrl = 'http://localhost:4566/000000000000/dev-club-localstack-queue';
 const main = async () => {
   for (let i = 0; i < 100; i++) {
     const message = generateMessage();
-    sendMessage(queueUrl, message);
+    sendMessageToSQS(queueUrl, message);
+    await new Promise((resolve) => setTimeout(resolve, Math.random() * 5000));
   }
 };
 
