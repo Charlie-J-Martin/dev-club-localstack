9b39730b2bbf1239b75260072e29d6e321948716
Charlie Martin <56304371+Charlie-J-Martin@users.noreply.github.com>
2024-03-15 17:48:00 +0000

    Delete packages/message-processor directory

--- packages/message-processor/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { pollQueue } from './processMessage';
-
-const queueUrl = 'http://localhost:4566/000000000000/dev-club-localstack-queue';
-
-const main = async () => {
-  pollQueue(queueUrl);
-};
-
-main();
